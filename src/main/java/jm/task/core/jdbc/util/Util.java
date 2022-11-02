9d9cc4ba66057aa5a81ce0111f73c5436b4f5627
JaneTyur245 <janestump@mail.ru>
2022-11-02 18:38:48 +0300

    TEST3

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,12 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
@@ -11,39 +18,32 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class Util {
-    private static Connection conn = null;
-    private static Util instance = null;
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String HOST = "jdbc:mysql://localhost:3306/test?useSSL=false&allowMultiQueries=true&serverTimezone=UTC";
+    private static final String LOGIN = "root";
+    private static final String PASSWORD = "root";
+    private static SessionFactory sessionFactory = null;
 
-    private Util() {
+    public static SessionFactory getConnection() {
+
         try {
-            if (null == conn || conn.isClosed()) {
-                Properties props = getProps();
-                conn = DriverManager
-                        .getConnection(props.getProperty("db.url"), props.getProperty("db.username"), props.getProperty("db.password"));
-            }
-        } catch (SQLException | IOException e) {
+            Configuration configuration = new Configuration()
+                    .setProperty("hibernate.connection.driver_class", DRIVER)
+                    .setProperty("hibernate.connection.url", HOST)
+                    .setProperty("hibernate.connection.username", LOGIN)
+                    .setProperty("hibernate.connection.password", PASSWORD)
+                    .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                    .addAnnotatedClass(User.class)
+                    .setProperty("hibernate.c3p0.min_size","5")
+                    .setProperty("hibernate.c3p0.max_size","200")
+                    .setProperty("hibernate.c3p0.max_statements","200");
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (HibernateException e) {
             e.printStackTrace();
         }
-    }
-
-    public static Util getInstance() {
-        if (null == instance) {
-            instance = new Util();
-        }
-        return instance;
-    }
-
-    public Connection getConnection() {
-        return conn;
-    }
-
-    private static Properties getProps() throws IOException {
-        Properties props = new Properties();
-        try (InputStream in = Files.newInputStream(Paths.get(Util.class.getResource("/database.properties").toURI()))) {
-            props.load(in);
-            return props;
-        } catch (IOException | URISyntaxException e) {
-            throw new IOException("Database config file not found", e);
-        }
+        return sessionFactory;
     }
 }
